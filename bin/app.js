6f61d1219d9d29a5c40bc20f978f50f91987f55e
BUON Jean-Guy <jeanguy.buon@gmail.com>
2016-04-04 16:33:01 +0200

    Hello World

--- bin/app.js
+++ bin/app.js
@@ -2,4 +2,11 @@
 "use strict";
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 3000;
+app.get("/", function (req, res) {
+    res.send("Hello World");
+});
+app.listen(port, function () {
+    console.log("server started on port " + port);
+});
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -3,3 +3,13 @@
 import * as express from 'express'
 
 var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+app.listen(port, () => {
+    console.log("server started on port "+port);
+});
