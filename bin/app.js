4a66eb42995724211ff4e8a09c9641858ca4a6ae
BUON Jean-Guy <jeanguy.buon@gmail.com>
2016-04-04 15:49:26 +0200

    npm and typings installation

---
+++ bin/app.js
@@ -0,0 +1,5 @@
+/// <reference path="../typings/main.d.ts" />
+"use strict";
+var express = require('express');
+var app = express();
+//# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,5 @@
+/// <reference path="../typings/main.d.ts" />
+
+import * as express from 'express'
+
+var app = express();
